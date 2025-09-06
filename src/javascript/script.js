d5366531d83673801ca3f9a9c871aecb65bf93ae
luibiondo <luigibiondo2207@gmail.com>
2025-09-06 13:18:03 -0300

    Deixando o header fixo e colocando animação quando scrolla

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,20 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function(){
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-bars fa-xmark');
     });
 });
+
+// ANIMATION HEADER
+// Pega o header
+const header = document.querySelector("header");
+
+// Escuta o scroll
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
